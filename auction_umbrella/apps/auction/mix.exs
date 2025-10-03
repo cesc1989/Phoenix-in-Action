2e47b5b6ba04005a3d297d2a75cfb1ef5b577e5c
Francisco Quintero Coronell <frajaquico@yahoo.es>
2025-10-03 15:28:56 -0500

    install comeonin and pbkdf2

--- auction_umbrella/apps/auction/mix.exs
+++ auction_umbrella/apps/auction/mix.exs
@@ -27,7 +27,9 @@ defmodule Auction.MixProject do
   defp deps do
     [
       {:ecto_sql, "3.12.0"},
-      {:postgrex, "0.19.1"}
+      {:postgrex, "0.19.1"},
+      {:comeonin, "~> 4.1"},
+      {:pbkdf2_elixir, "~> 0.12"}
     ]
   end
 end
