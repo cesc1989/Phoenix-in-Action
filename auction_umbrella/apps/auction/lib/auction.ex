511c0fec2734c671e04799def274e3535be6f0f6
Francisco Quintero Coronell <frajaquico@yahoo.es>
2025-10-07 23:17:15 -0500

    Completa inicio de sesión

--- auction_umbrella/apps/auction/lib/auction.ex
+++ auction_umbrella/apps/auction/lib/auction.ex
@@ -1,5 +1,5 @@
 defmodule Auction do
-  alias Auction.{Item, User}
+  alias Auction.{Item, User, Password}
 
   # Instead of calling Auction.FakeRepo directly in each function,
   # you can set your preferred repo at the top of the file as a module attribute
@@ -46,5 +46,13 @@ defmodule Auction do
     %User{}
     |> User.changeset_with_password(params)
     |> @repo.insert
+  end
+
+  def get_user_by_username_and_password(username, password) do
+    with user when not is_nil(user) <- @repo.get_by(User, %{username: username}),
+      true <- Password.verify_with_hash(password, user.hashed_password) do user
+    else
+      _ -> Password.dummy_verify
+    end
   end
 end
--- auction_umbrella/apps/auction_web/lib/auction_web/controllers/session_controller.ex
+++ auction_umbrella/apps/auction_web/lib/auction_web/controllers/session_controller.ex
@@ -5,8 +5,19 @@ defmodule AuctionWeb.SessionController do
     render(conn, :new)
   end
 
-  def create(conn, %{"user" => user}) do
-    # later
+  def create(conn, %{"user" => %{"username" => username, "password" => password}}) do
+    case Auction.get_user_by_username_and_password(username, password) do
+      %Auction.User{} = user ->
+        conn
+        |> put_session(:user_id, user.id)
+        |> put_flash(:info, "Successfully logged in")
+        |> redirect(to: ~p"/users/#{user}")
+
+        _ ->
+          conn
+          |> put_flash(:error, "That username and password combination cannot be found")
+          |> render(:new)
+    end
   end
 
   def delete(conn, _params) do
