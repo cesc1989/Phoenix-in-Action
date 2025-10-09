178ac16174a753ed9459bad7ba95e643a6306ce8
Francisco Quintero Coronell <frajaquico@yahoo.es>
2025-10-09 10:20:25 -0500

    restrict users opening another user page

--- auction_umbrella/apps/auction_web/lib/auction_web/controllers/user_controller.ex
+++ auction_umbrella/apps/auction_web/lib/auction_web/controllers/user_controller.ex
@@ -1,6 +1,8 @@
 defmodule AuctionWeb.UserController do
   use AuctionWeb, :controller
 
+  plug :prevent_unauthorized_access when action in [:show]
+
   def show(conn, %{"id" => id}) do
     user = Auction.get_user(id)
     render(conn, :show, user: user)
@@ -16,6 +18,24 @@ defmodule AuctionWeb.UserController do
       {:ok, user} -> redirect(conn, to: ~p"/users/#{user}")
 
       {:error, user} -> render(conn, :new, user: user)
+    end
+  end
+
+  defp prevent_unauthorized_access(conn, opts) do
+    current_user = Map.get(conn.assigns, :current_user)
+
+    requested_user_id =
+      conn.params
+      |> Map.get("id")
+      |> String.to_integer()
+
+    if current_user == nil || current_user.id != requested_user_id do
+      conn
+      |> put_flash(:error, "Nice try, encino man")
+      |> redirect(to: ~p"/items")
+      |> halt()
+    else
+      conn
     end
   end
 end
