706fa9d759230379a0638b1ec6a8f0817c551963
Francisco Quintero Coronell <frajaquico@yahoo.es>
2025-10-09 09:42:15 -0500

    Intro Authenticator plug

--- auction_umbrella/apps/auction_web/lib/auction_web/router.ex
+++ auction_umbrella/apps/auction_web/lib/auction_web/router.ex
@@ -8,6 +8,8 @@ defmodule AuctionWeb.Router do
     plug :put_root_layout, html: {AuctionWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+
+    plug AuctionWeb.Authenticator
   end
 
   pipeline :api do
